bb2335622d6038bef258d7d51f2d9c5a25d7af39
Elleni-Bisrat <ellenibisrat16@gmail.com>
2025-03-10 20:26:36 +0300

    props and state

---
+++ task-2/src/App.js
@@ -0,0 +1,37 @@
+import "./App.css";
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  const counterhandeler = () => {
+    setCount((s) => s + 1);
+  };
+
+  return (
+    <div className="App">
+      <Props name="Elleni Bisrat" email="ellenibisrat94@gmail.com" age="21" />
+      <p className="count_btn">count : {count}</p>
+      <button onClick={counterhandeler} className="countChange">
+        Count
+      </button>
+    </div>
+  );
+}
+const Props = (props) => {
+  return (
+    <div className="container">
+      <p>
+        Name: <span style={{ color: "darkblue" }}>{props.name}</span>{" "}
+      </p>
+      <p>
+        Email: <span style={{ color: "yellow" }}>{props.email}</span>{" "}
+      </p>
+      <p>
+        Age: <span style={{ color: "purple" }}>{props.age}</span>
+      </p>
+      </div>
+  );
+};
+
+export default App;
